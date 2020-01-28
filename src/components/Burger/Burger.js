aca64ffc4d6be0688b95e11a17b9fd70cf75b8cd
Ahmed AbuQamar <devqmr@gmail.com>
2020-01-28 16:54:33 +0200

    Ask User to add Ingredients by check the Ingredients list if empty

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,14 +4,30 @@ import classes from './Burger.module.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const Burger = props => {
+
+
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])]
+                .map((_, i) => {
+                    console.log("igKey > " + igKey)
+                    console.log("i > " + i)
+                    return <BurgerIngredient key={igKey + i} type={igKey}/>
+                })
+        })
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p> Please Add Ingredients</p>
+    }
+
+    console.log("transformedIngredients > " + transformedIngredients)
     return (
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
-            <BurgerIngredient type="salad"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
